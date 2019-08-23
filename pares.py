800f9327a0582ff0b0580d9d81d21ba62dcb079c
julife12 <julian.jaimes.2019@upb.edu.cp>
2019-08-23 10:42:48 -0500

    correccion ejercicio pares

--- pares.py
+++ pares.py
@@ -2,13 +2,18 @@ a=int(input("digite la cantidad de numeros que desea probar: "))
 print("cuando un numero sea impar se detendra el proceso")
 cant_par=0
 cant_impar=0
+
 for i in range(1, a+1):
     num=int(input("digite el numero: "))
     if num%2==0:
         cant_par=cant_par+1        
     else:
         cant_impar=cant_impar+1
+
+    
+    if num==0:
         break
+    
 
 
 print("la cantidad de numeros pares es: ", cant_par, "el numero impar fue: ", num)
