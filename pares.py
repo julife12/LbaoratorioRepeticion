0688c5a5079984d5d468f86c41e4dbb080019aef
julife12 <julian.jaimes.2019@upb.edu.cp>
2019-08-23 09:26:26 -0500

    Ejercicio dos resuelto

--- pares.py
+++ pares.py
@@ -1,5 +1,14 @@
-a=int(input("digite el numero: "))
-if a%2==0:
-    print("el numero ", a, " es par")
-else:
-    print("el numero ", a, " no es par")
+a=int(input("digite la cantidad de numeros que desea probar: "))
+print("cuando un numero sea impar se detendra el proceso")
+cant_par=0
+cant_impar=0
+for i in range(1, a+1):
+    num=int(input("digite el numero: "))
+    if num%2==0:
+        cant_par=cant_par+1        
+    else:
+        cant_impar=cant_impar+1
+        break
+
+
+print("la cantidad de numeros pares es: ", cant_par, "el numero impar fue: ", num)
