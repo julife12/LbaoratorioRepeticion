275ea4540e5ed452902ec31cef429222d15736e5
julife12 <julian.jaimes.2019@upb.edu.cp>
2019-08-23 10:49:27 -0500

    correccion ejercicio paresv2

--- pares.py
+++ pares.py
@@ -5,15 +5,15 @@ cant_impar=0
 
 for i in range(1, a+1):
     num=int(input("digite el numero: "))
+    if num==0:
+        break
+
+
     if num%2==0:
         cant_par=cant_par+1        
     else:
         cant_impar=cant_impar+1
-
-    
-    if num==0:
-        break
     
 
 
-print("la cantidad de numeros pares es: ", cant_par, "el numero impar fue: ", num)
+print("la cantidad de numeros pares es: ", cant_par, "la cantidad de numeros impares es: ", cant_impar)
